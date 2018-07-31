ae3ec257096e4f55cd15788c0409379a64f201f4
Ruth <github email address>
2018-07-31 11:56:25 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function produceDrivingRange(distance) {
+  return function (block1, block2) {
+    let outOfRange = parseInt(block2) - parseInt(block1) > distance
+    if (outOfRange === true) {
+      return parseInt(block2) - parseInt(block1) - distance + " blocks out of range"
+    } else {
+      return `within range by ${distance - (parseInt(block2) - parseInt(block1))}`
+    }
+  }
+}
+
+function produceTipCalculator(tip) {
+  return function (fare) {
+    return tip * fare
+  }
+}
+
+function createDriver(){
+  let driverId = 0
+
+  return class {
+    constructor(name) {
+      this.name = name
+      this.id = ++driverId
+    }
+  }
+}
